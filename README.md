957175d0ba124669861430be6d7c9f1bb9f32460
Dustin Smith <dustin@media.mit.edu>
2014-04-24 00:13:16 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-locallisten
-===========
+# Local Listen
 
-Finds popular songs by the bands who will soon play nearby
+This simple script finds the a few popular songs (on (Google Play)[http://music.google.com]) of the bands that are performing nearby within the next week---giving you time to decide if you want to go to the show and listen to them perform.
+
+To use this script you need to have:
+  
+  1. Credentials to login to an "All Access" Google Play subscription
+  2. An API key for JamBase, which you can sign up for at
+     http://developer.jambase.com/page
+
+This script depends on the following Python libraries:
+  
+    gmusicapi requests gitpass
+
+Dustin Smith  
+2014/4/23
+dustin@media.mit.edu
---
+++ locallisten.py
@@ -0,0 +1,89 @@
+""" 
+LocalListen - https://github.com/dasmith/locallisten
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Dustin Smith (dustin@media.mit.edu)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+#--------------------------------------------------------------------
+# set these parameters
+zipcode = "02139"
+radius_in_miles = "5"
+name_of_playlist = "! Bands playing in Cambridge, MA"
+#--------------------------------------------------------------------
+import re
+import requests
+import gitpass
+from gmusicapi import Mobileclient
+
+def get_and_clear_playlist(name_of_playlist):
+    """ Returns the playlist after deleting the songs"""
+    playlist = None
+    for play in gapi.get_all_user_playlist_contents():
+        if play['name'] == name_of_playlist and not play['deleted']:
+            pid= play['id']
+            # delete the existing songs
+            for track in play['tracks']:
+                gapi.remove_entries_from_playlist(track['id'])
+            return pid
+    return gapi.create_playlist(name_of_playlist)
+
+
+jambase_api_key = gitpass.gitpass("Enter your Jambase API key")
+
+gapi = Mobileclient()
+gapi.login(gitpass.gitpass("Enter your Google Play email: "),
+           gitpass.gitpass("Enter your Google Play password: "))
+    
+playlist_id = get_and_clear_playlist(name_of_playlist)
+
+data = {'zipCode': zipcode,
+        'radius': radius_in_miles,
+        'page': 0,
+        'api_key': jambase_api_key}
+
+response = requests.get("http://api.jambase.com/events", params=data)
+data = response.json()
+
+for event in data['Events']:
+    for artist in event['Artists']:
+        artist_name = artist['Name']
+        print "\n", artist_name, "@", event['Venue']['Name'],
+        
+        # search Google All Access for the artist
+        result = gapi.search_all_access(artist_name)
+        
+        # look at the first result
+        for google_artist_data in result.get('artist_hits', []):
+            google_artist = google_artist_data['artist']
+            google_artist_id = google_artist['artistId']
+            print "----> %s (%0.2f)" % (google_artist['name'], google_artist_data['score'])
+            if google_artist_data['score'] > 200:
+                # TODO: confirm high string similarity, as sometimes Google gives high scores to 
+                # strange matches.
+                song_data = gapi.get_artist_info(google_artist_id, include_albums=False, max_top_tracks=5, max_rel_artist=0)
+                for top_track in song_data.get('topTracks', []):
+                    #if top_track['genre'] in banned_genres:
+                    #   continue 
+                    print " + %s [%s]" (top_track['title'], top_track['genre'])
+                    song_id = top_track.get('id') or top_track.get('nid')
+                    gapi.add_songs_to_playlist(playlist_id, song_id)
+            break
